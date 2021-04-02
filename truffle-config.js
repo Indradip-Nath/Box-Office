864d2cb8ae5417e915cd2c9fda1dabea95c0bbff
Indradip Nath <61374965+Indradip-Nath@users.noreply.github.com>
2021-04-02 14:02:34 +0530

    Add files via upload

---
+++ truffle-config.js
@@ -0,0 +1,33 @@
+const { HOST, PORT } = require("./config");
+const { adminProvider } = require("./utils/web3");
+
+module.exports = {
+  migrations_directory: "./migrations",
+  networks: {
+    ganache: {
+      host: "localhost",
+      port: 7545,
+      network_id: "*",
+      // gas: 8003877,
+      // gasPrice: 2300000000
+      gas: 99999999,
+      //gasPrice: 1
+    },
+    rinkeby: {
+      provider: () => adminProvider,
+      network_id: 4
+    }
+  },
+  compilers: {
+    solc: {
+       version: "0.4.24",    // Fetch exact version from solc-bin (default: truffle's version)
+        optimizer: {
+          enabled: true,
+          runs: 5000
+        },
+    },
+  },
+  mocha: {
+    useColors: true
+  } 
+};
